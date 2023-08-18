4ded96d7041a81d659d2a4ea651fcb7985b429f9
arnold-O <arnoldoghiator@gmail.com>
2023-08-19 00:35:11 +0100

    error handling

--- server.js
+++ server.js
@@ -1,81 +1,55 @@
-const express = require('express');
-const dotenv = require('dotenv')
-const axios = require('axios');
+const express = require("express");
+const dotenv = require("dotenv");
+const axios = require("axios");
 dotenv.config({ path: "./.env" });
 const app = express();
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./utils/errorController");
 
+app.get("/flights", async (req, res, next) => {
+  const filters = req.query;
+  const options = {
+    method: "GET",
+    url: process.env.MOCK_API,
+  };
+  const response = await axios.request(options);
 
-app.get('/flights', async(req, res)=>{
-    const filters = req.query;
-    // console.log(filters)
-    const check = Object.keys(filters)
-    const answer = Object.values(filters)
-   
+  const queryObj = { ...filters };
 
-    const options = {
-        method: 'GET',
-        url: 'https://mocki.io/v1/c6022b01-1b3b-419a-ba19-75c44838424b',
-      };
-      
-   
-          const response = await axios.request(options);
-         
-          const filteredUsers = response.data.filter(user => {
-           let data = []
-            for (key in filters) {
-            //   console.log(key, user[key], filters[key]);
-              data =  user[key] == filters[key];
-            }
-            return data;
-          });
-        
-         if(filteredUsers.length > 1){
-            res.json(filteredUsers);
-        }else{
-           
-            res.json({
-                message:`This ${check} with value ${answer} is not valid`
-            });
+  let queryStr = JSON.stringify(queryObj);
+  queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `${match}`);
 
-         }
+  //   const filters = req.query;
+  const filteredUsers = response.data.filter((user) => {
+    let isValid = true;
+    for (key in filters) {
+      //   console.log(key, user[key], filters[key]);
+      isValid = isValid && user[key] == filters[key];
+    }
+    return isValid;
+  });
 
+  if (filteredUsers.length > 0) {
+    return res.status(200).json(filteredUsers);
+  } else {
+    return next(
+      new AppError(
+        `The ${Object.keys(filters)} field of value ${Object.values(
+          filters
+        )} is not valid`,
+        400
+      )
+    );
+  }
+});
 
+app.all("*", (req, res, next) => {
+  next(new AppError(`can't find ${req.originalUrl} on this server`, 404));
+});
 
-      
-    
-  
-
-})
-// app.get('/flights', async(req, res)=>{
-    
-
-//     const options = {
-//       method: 'GET',
-//       url: 'https://toronto-pearson-airport.p.rapidapi.com/arrivals/carousel/9',
-//       headers: {
-//         'X-RapidAPI-Key': '5d52450d40msh9d1af2bbd083218p1f611ejsne64c0d9899cb',
-//         'X-RapidAPI-Host': 'toronto-pearson-airport.p.rapidapi.com'
-//       }
-//     };
-    
-//     try {
-//         const response = await axios.request(options);
-//         console.log(response.data);
-//     } catch (error) {
-//         console.error(error);
-//     }
-    
-  
-// })
-
-
-
-
-
-
-
+app.use(globalErrorHandler);
 
 const PORT = 3000;
-app.listen(PORT, ()=>{
-    console.log(`app running on port ${PORT}`)
-})
+app.listen(PORT, () => {
+  console.log(`app running on port ${PORT}`);
+});
---
+++ utils/errorController.js
@@ -0,0 +1,16 @@
+
+module.exports = (err, req, res, next) => {
+  err.statusCode = err.statusCode || 500;
+  err.status = err.status || "error";
+
+
+  res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    stack: err.stack,
+    name:err.name,
+    err,
+  });
+
+  
+};
