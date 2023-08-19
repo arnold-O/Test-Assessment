2d2b5193dceb62b04f20c4dcd68dd8b9a375c763
arnold-O <arnoldoghiator@gmail.com>
2023-08-19 12:07:18 +0100

    adding documentation

--- server.js
+++ server.js
@@ -1,10 +1,19 @@
 const express = require("express");
+const app = express();
 const dotenv = require("dotenv");
 const axios = require("axios");
 dotenv.config({ path: "./.env" });
-const app = express();
 const AppError = require("./utils/appError");
 const globalErrorHandler = require("./utils/errorController");
+
+        // Swagger documentation
+        const swaggerUi = require("swagger-ui-express");
+        const YAML = require("yamljs");
+        const swaggerDocument = YAML.load("./swagger.yaml")
+
+        
+        
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 app.get("/flights", async (req, res, next) => {
   const filters = req.query;
@@ -12,9 +21,47 @@ app.get("/flights", async (req, res, next) => {
     method: "GET",
     url: process.env.MOCK_API,
   };
+
   const response = await axios.request(options);
 
-  const queryObj = { ...filters };
+  const queryObj = { ...filters};
+
+  let queryStr = JSON.stringify(queryObj);
+  queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `${match}`);
+
+  //   const filters = req.query;
+  const filteredUsers = response.data.filter((user) => {
+    let isValid = true;
+    for (key in filters) {
+      //   console.log(key, user[key], filters[key]);
+      isValid = isValid && user[key] == filters[key];
+    }
+    return isValid;
+  });
+
+  if (filteredUsers.length > 0) {
+    return res.status(200).json(filteredUsers);
+  } else {
+    return next(
+      new AppError(
+        `The ${Object.keys(filters)} field of value ${Object.values(
+          filters
+        )} is not valid`,
+        400
+      )
+    );
+  }
+});
+app.get("/flights/query", async (req, res, next) => {
+  const filters = req.query;
+  const options = {
+    method: "GET",
+    url: process.env.MOCK_API,
+  };
+
+  const response = await axios.request(options);
+
+  const queryObj = { ...filters};
 
   let queryStr = JSON.stringify(queryObj);
   queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `${match}`);
@@ -53,3 +100,5 @@ const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`app running on port ${PORT}`);
 });
+
+
